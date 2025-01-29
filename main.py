9b752b729a97acb7225a072767098a6f7df96ef7
Davi M. <163705195+DaviMeurer@users.noreply.github.com>
2025-01-28 21:46:26 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv #chamado os módulos e classes
 from bs4 import BeautifulSoup
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -6,43 +6,60 @@ import requests
 import smtplib
 import os
 
-url = "https://store.steampowered.com/app/1446780/MONSTER_HUNTER_RISE/"
+load_dotenv() #puxando dados do .env
 
-try:
-    resposta = requests.get(url) #pega o link do site
-    resposta.raise_for_status() #checa se conectou com suscesso
+url = "https://panini.com.br/berserk-edicao-de-luxo-vol-1-amaxs001r6"
 
-    if resposta.status_code == 200: #foi um sucesso?
-        conteudo = BeautifulSoup(resposta.content, "html.parser")
+def consulta_preco():
+    try:
+        resposta = requests.get(url) #pega o link do site
+        resposta.raise_for_status() #checa se conectou com suscesso
 
-        preco_base = conteudo.find("div", class_ = "discount_original_price")
-        preco_desconto = conteudo.find("div", class_ = "discount_final_price")
+        if resposta.status_code == 200: #foi um sucesso?
+            conteudo = BeautifulSoup(resposta.content, "html.parser")
 
-        valor = preco_base.value
-        if valor == preco_base.value:
-            print("Jogo em promoção, de", preco_base.get_text(), "para", preco_desconto.get_text())
-            
-except requests.exceptions.RequestException as e:
-    print("Erro ao se conectar ao site", e)
-except Exception as e:
-    print("Erro desconhecido", e)
+            preco_base = conteudo.find("span", class_ = "price")
 
-def enviar_email():
-    destinatario = os.getenv("DESTINATARIO")
+            valor = preco_base.value
+            if valor != preco_base.value:
+                print("Berserk Vol.1 está em promoção.")
+            else:
+                print("Não está em promoção.")
+
+                return valor
+                
+    except requests.exceptions.RequestException as e:
+        print("Erro ao se conectar ao site", e)
+    except Exception as e:
+        print("Erro desconhecido", e)
+
+def enviar_email(envio):
+    destinatario = os.getenv("DESTINATARIO") #criando variáveis com dados de login
     remetente = os.getenv("SMTP_USER")
     senha = os.getenv("SMTP_PASSWORD")
     servidor_email = os.getenv("SMTP_SERVER")
     porta = os.getenv("SMTP_PORT")
-    titulo = "Monster Hunter Rise em desconto!"
+    titulo = "Berserk vol. 1 em promoção!"
 
-    corpo = f"O jogo de {preco_base} para {preco_desconto}" #pequeno texto abaixo do título
+    corpo = "Volume de Berserk está com desconto.\n\n"
+    corpo += "link para a compra: https://encr.pw/berserkvol1" #pequeno texto abaixo do título
 
-    msg = MIMEMultipart()
+    msg = MIMEMultipart() #estruturar as variáveis para o email
     msg["From"] = remetente
     msg["To"] = destinatario
     msg["Subject"] = titulo
     msg.attach(MIMEText(corpo, "plain"))
 
-    try:
-        with smtplib.SMT(servidor_email, porta) as server:
+    try: #tentará conectar ao seu e-mail
+        with smtplib.SMTP(servidor_email, porta) as server: #estabelecer uma conexão com o destinatário
             server.starttls()
+            server.login(remetente, senha)
+            server.sendmail(remetente, destinatario, msg.as_string())
+            print("Promoção de jogo enviado.")
+            
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
+
+#main
+if __name__ == "__main__":
+    enviar_email(consulta_preco())
